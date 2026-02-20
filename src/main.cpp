e701573bda4fb268a43a904e8db1414f8edc287f
Madhav2005-Garg <madhavgarg545@gmail.com>
2026-02-20 21:58:42 +0530

    configure serial interface and boot sequence

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,12 @@
 int ldrValue = 0;
 int threshold = 500; // Calibrated for average room light
 
+// ... definitions from previous commit ...
+
 void setup() {
+    Serial.begin(9600);
+    Serial.println("LDR Light Intensity Monitoring System - Initialized");
+    delay(1000);
 }
 
 void loop() {
